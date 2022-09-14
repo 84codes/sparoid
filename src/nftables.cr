8d4eb734eb567ca34ff090a2e368021417b5c218
Carl Hörberg <carl@84codes.com>
2022-09-14 13:03:59 +0200

    some versions of nft -i also leaks memory
    reverting to executing one nft at a time

--- src/nftables.cr
+++ src/nftables.cr
@@ -1,17 +1,14 @@
 # Class to interact with nftables
 # Not linked to libnftables, but calls out to the `nft` binary
 class Nftables
-  def initialize
-    input, @io = IO.pipe
-    nft = Process.new("nft", {"-i"}, input: input, output: Process::Redirect::Inherit, error: Process::Redirect::Inherit)
-    spawn do
-      nft.wait
-      abort "nft exited"
-    end
+  def self.run_cmd(cmd : String) : Nil
+    status = Process.run("nft", {cmd}, output: Process::Redirect::Inherit, error: Process::Redirect::Inherit)
+    status.success? || raise Error.new("nftables command '#{cmd}' failed")
   end
 
-  def run_cmd(cmd : String) : Nil
-    @io.puts cmd
+  def self.run_file(file : String) : Nil
+    status = Process.run("nft", {"-f", file}, output: Process::Redirect::Inherit, error: Process::Redirect::Inherit)
+    status.success? || raise Error.new("nftables file '#{file}' failed")
   end
 
   class Error < Exception; end
