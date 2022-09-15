0b1f739741df9f4279ddd5780d37296527b5fc42
Carl Hörberg <carl@84codes.com>
2022-09-15 20:56:07 +0200

    Bundle libnftables in deb build

--- deb.dockerfile
+++ deb.dockerfile
@@ -1,7 +1,28 @@
 ARG build_image
 FROM $build_image AS build-stage
-WORKDIR /build
 
+# build static libmnl
+WORKDIR /tmp/libmnl
+RUN curl -sL https://www.netfilter.org/pub/libmnl/libmnl-1.0.5.tar.bz2 | tar jx --strip-components=1 && \
+  ./configure --enable-static --disable-shared --prefix=/usr && \
+  make -j CFLAGS="-fPIE -O3" && \
+  make install
+
+# build static libnftnl
+WORKDIR /tmp/libnftnl
+RUN curl -sL https://www.netfilter.org/pub/libnftnl/libnftnl-1.2.3.tar.bz2 | tar jx --strip-components=1 && \
+  ./configure --enable-static --disable-shared --prefix=/usr && \
+  make -j CFLAGS="-fPIE -O3" && \
+  make install
+
+# build static nftables
+WORKDIR /tmp/nftables
+RUN curl -sL https://www.netfilter.org/pub/nftables/nftables-1.0.5.tar.bz2 | tar jx --strip-components=1 && \
+  ./configure --enable-static --disable-shared --with-mini-gmp --without-cli --prefix=/usr && \
+  make -j CFLAGS="-fPIE -O3" && \
+  make install
+
+WORKDIR /tmp/sparoid
 # Copy all files
 COPY shard.yml shard.lock README.md LICENSE ./
 COPY src src
@@ -12,4 +33,4 @@ RUN build/deb
 
 # Copy the deb package to a scratch image, that then can be exported
 FROM scratch AS export-stage
-COPY --from=build-stage /build/builds .
+COPY --from=build-stage /tmp/sparoid/builds .
--- src/nftables.cr
+++ src/nftables.cr
@@ -1,15 +1,60 @@
-# Class to interact with nftables
-# Not linked to libnftables, but calls out to the `nft` binary
-class Nftables
-  def self.run_cmd(cmd : String) : Nil
-    status = Process.run("nft", {cmd}, output: Process::Redirect::Inherit, error: Process::Redirect::Inherit)
-    status.success? || raise Error.new("nftables command '#{cmd}' failed")
+{% if flag?(:without_nftables) %}
+  # Class to interact with nftables
+  # Not linked to libnftables, but calls out to the `nft` binary
+  class Nftables
+    def run_cmd(cmd : String) : Nil
+      status = Process.run("nft", {cmd}, output: Process::Redirect::Inherit, error: Process::Redirect::Inherit)
+      status.success? || raise Error.new("nftables command '#{cmd}' failed")
+    end
+
+    def run_file(file : String) : Nil
+      status = Process.run("nft", {"-f", file}, output: Process::Redirect::Inherit, error: Process::Redirect::Inherit)
+      status.success? || raise Error.new("nftables file '#{file}' failed")
+    end
+
+    class Error < Exception; end
+  end
+{% else %}
+  # Class to interact with nftables
+  # All output is printed to stdout/stderr
+  class Nftables
+    def initialize
+      @nft = LibNftables.nft_ctx_new(LibNftables::NFT_CTX_DEFAULT)
+    end
+
+    def finalize
+      LibNftables.nft_ctx_free(@nft)
+    end
+
+    # Execute a nft command, eg. 'list ruleset'
+    def run_cmd(cmd : String) : Nil
+      LibNftables.nft_run_cmd_from_buffer(@nft, cmd).zero? ||
+        raise Error.new("nftables command '#{cmd}' failed")
+    end
+
+    # Execute a nft script in a file
+    def run_file(file : String) : Nil
+      LibNftables.nft_run_cmd_from_filename(@nft, file).zero? ||
+        raise Error.new("nftables file '#{file}' failed")
+    end
+
+    class Error < Exception; end
   end
 
-  def self.run_file(file : String) : Nil
-    status = Process.run("nft", {"-f", file}, output: Process::Redirect::Inherit, error: Process::Redirect::Inherit)
-    status.success? || raise Error.new("nftables file '#{file}' failed")
+  # https://git.netfilter.org/nftables/plain/doc/libnftables.adoc
+  # https://git.netfilter.org/nftables/tree/include/nftables/libnftables.h
+  @[Link("mnl")]
+  @[Link("nftnl")]
+  @[Link("nftables")]
+  lib LibNftables
+    NFT_CTX_DEFAULT = 0u32
+    fun nft_ctx_new(flags : UInt32) : NftCtx*
+    fun nft_ctx_free(ctx : NftCtx*) : Void
+    fun nft_run_cmd_from_buffer(ctx : NftCtx*, buf : LibC::Char*) : LibC::Int
+    fun nft_run_cmd_from_filename(ctx : NftCtx*, buf : LibC::Char*) : LibC::Int
   end
 
-  class Error < Exception; end
-end
+  @[Extern]
+  struct NftCtx
+  end
+{% end %}
