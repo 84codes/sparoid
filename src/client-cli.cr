edeccfa816c950e2b78429ed6492f7b4a5fccc88
Carl Hörberg <carl@84codes.com>
2022-10-17 17:50:29 +0200

    Do ini parsing in Sparoid::Cient, not in CLI

--- src/client-cli.cr
+++ src/client-cli.cr
@@ -1,15 +1,12 @@
-require "ini"
 require "option_parser"
 require "./client"
 require "./version"
 
 subcommand = :none
-key = ENV.fetch("SPAROID_KEY", "")
-hmac_key = ENV.fetch("SPAROID_HMAC_KEY", "")
 host = "0.0.0.0"
 port = 8484
 tcp_port = 22
-config_path = File.expand_path "~/.sparoid.ini", home: true
+config_path = "~/.sparoid.ini"
 
 parser = OptionParser.new do |p|
   p.banner = "Usage: #{PROGRAM_NAME} [subcommand] [arguments]"
@@ -35,7 +32,7 @@ parser = OptionParser.new do |p|
     p.banner = "Usage: #{PROGRAM_NAME} send [arguments]"
     p.on("-h HOST", "--host=HOST", "Host to connect to") { |v| host = v }
     p.on("-p PORT", "--port=PORT", "UDP port") { |v| port = v.to_i }
-    p.on("-c PATH", "--config=PATH", "Path to config file") { |v| config_path = File.expand_path(v, home: true) }
+    p.on("-c PATH", "--config=PATH", "Path to config file") { |v| config_path = v }
   end
   p.on("connect", "Send a SPA, connect to a host/port and then pass the FD to parent") do
     subcommand = :connect
@@ -43,31 +40,19 @@ parser = OptionParser.new do |p|
     p.on("-h HOST", "--host=HOST", "Host to connect to") { |v| host = v }
     p.on("-p PORT", "--port=PORT", "UDP port") { |v| port = v.to_i }
     p.on("-P PORT", "--tcp-port=PORT", "TCP port") { |v| tcp_port = v.to_i }
-    p.on("-c PATH", "--config=PATH", "Path to config file") { |v| config_path = File.expand_path(v, home: true) }
+    p.on("-c PATH", "--config=PATH", "Path to config file") { |v| config_path = v }
   end
 end
 parser.parse
-
-if File.exists? config_path
-  config = File.open(config_path) { |f| INI.parse(f) }
-  config.each do |_, section|
-    section.each do |k, v|
-      case k
-      when "key"      then key = v
-      when "hmac-key" then hmac_key = v
-      end
-    end
-  end
-end
 
 begin
   case subcommand
   when :keygen
     Sparoid::Client.keygen
   when :send
-    Sparoid::Client.send(key, hmac_key, host, port)
+    Sparoid::Client.new(config_path).send(host, port)
   when :connect
-    ips = Sparoid::Client.send(key, hmac_key, host, port)
+    ips = Sparoid::Client.new(config_path).send(host, port)
     Sparoid::Client.fdpass(ips, tcp_port)
   else
     puts "Missed subcommand"
